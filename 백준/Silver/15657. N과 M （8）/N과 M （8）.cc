f0f0d6584b92078a4e2eb65a4acb7c8801e4c392
김태윤 <dnlfis42@gmail.com>
2025-08-18 07:02:42 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int m;
+
+int number[8];
+int order[8];
+
+static void recur(int level, int before)
+{
+	if (level == m)
+	{
+		for (int i = 0; i < m; ++i)
+		{
+			cout << order[i] << ' ';
+		}
+		cout << '\n';
+
+		return;
+	}
+
+	for (int i = before; i < n; ++i)
+	{
+		order[level] = number[i];
+		recur(level + 1, i);
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> number[i];
+	}
+	sort(number, number + n);
+
+	recur(0, 0);
+
+	return 0;
+}
