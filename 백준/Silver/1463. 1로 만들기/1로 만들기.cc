c7231944edaebeabfa76a5ecaa2a7466267fe387
김태윤 <dnlfis42@gmail.com>
2025-08-20 13:50:27 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -12,22 +12,25 @@ int main()
 
 	int n;
 	cin >> n;
-	memset(numbers, 100, sizeof(int) * (n + 1));
 	numbers[1] = 0;
-	for (int i = 1; i < n; ++i)
+	for (int i = 2; i <= n; ++i)
 	{
-		if (i * 3 <= 1000000 && numbers[i * 3] > numbers[i] + 1)
+		numbers[i] = numbers[i - 1];
+		if (i % 2 == 0)
 		{
-			numbers[i * 3] = numbers[i] + 1;
+			if (numbers[i] > numbers[i / 2])
+			{
+				numbers[i] = numbers[i / 2];
+			}
 		}
-		if (i * 2 <= 1000000 && numbers[i * 2] > numbers[i] + 1)
+		if (i % 3 == 0)
 		{
-			numbers[i * 2] = numbers[i] + 1;
+			if (numbers[i] > numbers[i / 3])
+			{
+				numbers[i] = numbers[i / 3];
+			}
 		}
-		if (numbers[i + 1] > numbers[i] + 1)
-		{
-			numbers[i + 1] = numbers[i] + 1;
-		}
+		++numbers[i];
 	}
 	cout << numbers[n];
 
