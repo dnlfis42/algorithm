7721bbc79079469ae4de525764d21bde3ee446dd
김태윤 <dnlfis42@gmail.com>
2025-09-12 19:48:18 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << "         ,r\'\"7\n"
+		<< "r`-_   ,\'  ,/\n"
+		<< " \\. \". L_r\'\n"
+		<< "   `~\\/\n"
+		<< "      |\n"
+		<< "      |";
+
+	return 0;
+}
