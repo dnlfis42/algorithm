448a9038f844b7377a20f76bb96eee40f30e2353
김태윤 <dnlfis42@gmail.com>
2025-09-17 16:01:59 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout << 
+		"\\    /\\\n" <<
+		" )  ( \')\n" <<
+		"(  /  )\n" <<
+		" \\(__)|";
+
+	return 0;
+}
+
+/*
+\    /\
+ )  ( ')
+(  /  )
+ \(__)|
+ */
