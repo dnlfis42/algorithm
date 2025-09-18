96b9d9da4f8067d6621fab892f5f6a5e7f0a4afe
김태윤 <dnlfis42@gmail.com>
2025-09-18 14:09:23 +0900

    [Gold IV] Title: 알파벳, Time: 444 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+constexpr int dr[] = { 0,1,0,-1 };
+constexpr int dc[] = { 1,0,-1,0 };
+
+char board[21][21];
+bool visit[21][21];
+bool alpha[26];
+
+int r;
+int c;
+
+int mx = 0;
+
+static void f(int ri, int ci, int cnt)
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		int tr = ri + dr[i];
+		int tc = ci + dc[i];
+
+		if (tr < 0 || tr >= r || tc < 0 || tc >= c)
+		{
+			continue;
+		}
+
+		if (visit[tr][tc] || alpha[board[tr][tc] - 'A'])
+		{
+			continue;
+		}
+
+		visit[tr][tc] = true;
+		alpha[board[tr][tc] - 'A'] = true;
+
+		f(tr, tc, cnt + 1);
+
+		visit[tr][tc] = false;
+		alpha[board[tr][tc] - 'A'] = false;
+	}
+
+	if (mx < cnt)
+	{
+		mx = cnt;
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+
+	cin >> r >> c;
+
+	for (int i = 0; i < r; ++i)
+	{
+		cin >> board[i];
+	}
+
+	visit[0][0] = true;
+	alpha[board[0][0] - 'A'] = true;
+	f(0, 0, 1);
+
+	cout << mx;
+
+	return 0;
+}
