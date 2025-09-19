c9636c0fa3edfc47c0df321167010cca1dce957c
김태윤 <dnlfis42@gmail.com>
2025-09-20 04:02:38 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	priority_queue<int> pq;
+
+	int n;
+	cin >> n;
+	while (n--)
+	{
+		int x;
+		cin >> x;
+
+		if (x == 0)
+		{
+			if (pq.empty())
+			{
+				cout << "0\n";
+			}
+			else
+			{
+				cout << pq.top() << "\n";
+				pq.pop();
+			}
+		}
+		else
+		{
+			pq.push(x);
+		}
+	}
+
+	return 0;
+}
