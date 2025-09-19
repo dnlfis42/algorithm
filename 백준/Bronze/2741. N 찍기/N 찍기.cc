4e345e24f81acaf514e755576d040cdbc665a4e7
김태윤 <dnlfis42@gmail.com>
2025-09-19 20:17:03 +0900

    [Bronze V] Title: N 찍기, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int n;
+	cin >> n;
+	int i = 1;
+	while (n--)
+	{
+		cout << i++ << "\n";
+	}
+
+	return 0;
+}
