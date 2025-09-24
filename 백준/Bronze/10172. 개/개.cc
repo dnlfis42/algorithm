41c1147d803cd8191d990cc1abd75cf9506ecf12
김태윤 <dnlfis42@gmail.com>
2025-09-25 08:51:14 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	cout <<
+		"|\\_/|\n" <<
+		"|q p|   /}\n" <<
+		"( 0 )\"\"\"\\\n" <<
+		"|\"^\"`    |\n" <<
+		"||_/=\\\\__|";
+
+	return 0;
+}
