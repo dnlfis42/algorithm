f2004ddec0d888a0e81b82feeb560d8ca56d970b
김태윤 <dnlfis42@gmail.com>
2025-09-26 15:06:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -19,27 +19,18 @@ int main()
 		cin >> stuff[i].first >> stuff[i].second;
 	}
 
-	vector<int> mxval(k + 1, 0);
+	vector<int> dp(k + 1, 0); // 배낭의 무게가 k일 때 최대로 넣을 수 있는 가치를 저장
 	for (int i = 0; i < n; ++i)
 	{
 		int w = stuff[i].first;
 		int v = stuff[i].second;
 
-		for (int j = 0; j <= k - w; ++j)
+		for (int j = k; j >= w; --j)
 		{
-			mxval[j] = max(mxval[j], v + mxval[j + w]);
+			dp[j] = max(dp[j], dp[j - w] + v);
 		}
 	}
-
-	int mx = 0;
-	for (int i = 0; i <= k; ++i)
-	{
-		if (mx < mxval[i])
-		{
-			mx = mxval[i];
-		}
-	}
-	cout << mx;
+	cout << dp[k];
 
 	return 0;
 }
