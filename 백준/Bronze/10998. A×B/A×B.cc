cd5ef0dab0cb18f896e805b9f59818c2e69a8d0d
김태윤 <dnlfis42@gmail.com>
2025-10-03 11:33:27 +0900

    [Bronze V] Title: A×B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a * b;
+
+	return 0;
+}
+
+int main()
+{
+	return solution();
+}
