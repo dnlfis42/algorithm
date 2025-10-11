306cc3725faac94d119eafa5475b11cfc8a180a6
김태윤 <dnlfis42@gmail.com>
2025-10-11 14:13:46 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 36 ms, Memory: 5728 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.cc
@@ -12,41 +12,64 @@ int solution()
 	int n; // 정점의 개수
 	int e; // 간선의 개수
 	cin >> n >> e;
+	vector<vector<pair<int, int>>> edges(n + 1);
 
-	vector<vector<int>> dist(n + 1, vector<int>(n + 1, INF));
-	for (int i = 1; i <= n; ++i)
-	{
-		dist[i][i] = 0;
-	}
 	for (int i = 0; i < e; ++i)
 	{
 		int a, b, c;
 		cin >> a >> b >> c;
-		dist[a][b] = c;
-		dist[b][a] = c;
+		edges[a].push_back({ c,b });
+		edges[b].push_back({ c,a });
 	}
+
+	auto min_dist = [&](int st, int en)
+		{
+			vector<int> dist(n + 1, INF);
+			dist[st] = 0;
+
+			priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq; // st부터 현재 좌표까지의 거리,현재 좌표
+			pq.push({ 0,st });
+
+			while (!pq.empty())
+			{
+				auto cur = pq.top();
+				pq.pop();
+
+				if (cur.first != dist[cur.second])
+				{
+					continue;
+				}
+
+				for (const auto& nxt : edges[cur.second])
+				{
+					if (dist[nxt.second] > cur.first + nxt.first)
+					{
+						dist[nxt.second] = cur.first + nxt.first;
+						pq.push({ dist[nxt.second],nxt.second });
+					}
+				}
+			}
+
+			return dist[en];
+		};
 
 	int v1, v2;
 	cin >> v1 >> v2;
 
-	for (int mid = 1; mid <= n; ++mid)
-	{
-		for (int st = 1; st <= n; ++st)
-		{
-			for (int en = 1; en <= n; ++en)
-			{
-				dist[st][en] = min(dist[st][en], dist[st][mid] + dist[mid][en]);
-			}
-		}
-	}
+	int st_v1 = min_dist(1, v1);
+	int st_v2 = min_dist(1, v2);
+	int v1_v2 = min_dist(v1, v2);
+	int v1_en = min_dist(v1, n);
+	int v2_en = min_dist(v2, n);
 
-	if (dist[1][v1] == INF || dist[1][v2] == INF || dist[v1][v2] == INF || dist[v1][n] == INF || dist[v2][n] == INF)
+	if (st_v1 == INF || st_v2 == INF || v1_v2 == INF || v1_en == INF || v2_en == INF)
 	{
 		cout << -1;
 		return 0;
 	}
 
-	cout << min(dist[1][v1] + dist[v2][n], dist[1][v2] + dist[v1][n]) + dist[v1][v2];
+	cout << min(st_v1 + v2_en, st_v2 + v1_en) + v1_v2;
+
 	return 0;
 }
 
