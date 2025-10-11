d33a659512f3b83c321268f12aff5cd395d31d92
김태윤 <dnlfis42@gmail.com>
2025-10-11 17:42:23 +0900

    [Bronze V] Title: WARBOY, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.cc
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+	cout << b / a * 3 * c;
+
+	return 0;
+}
+
+int main()
+{
+	return solution();
+}
